13572d59bceba2f50f039746145174ebf6dffaba
Alexey Patsovsky <alexey.patsovsky@gmail.com>
2024-11-18 20:27:52 +0300

    Removed href from li in breadcrumbs.

--- src/common/components/Breadcrumbs/index.tsx
+++ src/common/components/Breadcrumbs/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { BreadcrumbItem, Breadcrumbs as NextUIBreadcrumbs, BreadcrumbsProps as NextUIBreadcrumbsProps } from '@nextui-org/react';
 
 
@@ -12,8 +13,18 @@ interface BreadcrumbsProps extends Omit<NextUIBreadcrumbsProps, 'children'> {
 }
 
 export default function Breadcrumbs({ items, ...restProps }: BreadcrumbsProps) {
+    const navRef = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+        if (navRef.current) {
+            // for some reason NextUI adds href attribute to li node but that's invalid
+            Array.from(navRef.current.querySelectorAll('li')).forEach(liNode => liNode.removeAttribute('href'));
+        }
+    }, []);
+
     return (
         <NextUIBreadcrumbs
+            ref={navRef}
             color="primary"
             underline="hover"
             {...restProps}
